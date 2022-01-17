1a23cd2c22a47fec1061f282a655ba50d454c14e
TayoDavid <omotayo4david@gmail.com>
2022-01-18 00:04:35 +0100

    Design updated

--- CurrencyConverter/CurrencyConverterViewController.swift
+++ CurrencyConverter/CurrencyConverterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import DropDown
 
 class CurrencyConverterViewController: UIViewController {
 
@@ -21,13 +22,59 @@ class CurrencyConverterViewController: UIViewController {
     @IBOutlet weak var fromCurrencySymbolLabel: UILabel!
     @IBOutlet weak var targetCurrencySymbolLabel: UILabel!
     
+    @IBOutlet weak var fromCurrencyDropdownView: UIView!
+    @IBOutlet weak var targetCurrencyDropdownView: UIView!
+    
+    @IBOutlet weak var fromCurrencyLogoImgView: UIImageView!
+    @IBOutlet weak var selectedFromCurrencyLabel: UILabel!
+    
+    @IBOutlet weak var targetCurrencyLogoImgView: UIImageView!
+    @IBOutlet weak var selectedTargetCurrencyLabel: UILabel!
+    
+    @IBOutlet weak var convertButton: UIButton!
+    let fromCurrencyDropdown = DropDown()
+    let targetCurrencyDropdown = DropDown()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
         fromCurrencyView.layer.cornerRadius = 8
         targetCurrencyView.layer.cornerRadius = 8
+    
+        fromCurrencyDropdown.anchorView = fromCurrencyDropdownView
+        targetCurrencyDropdown.anchorView = targetCurrencyDropdownView
+    
+        fromCurrencyDropdown.dataSource = ["USD", "EURO", "PLN"]
+        targetCurrencyDropdown.dataSource = ["USD", "EURO", "PLN"]
+        
+        fromCurrencyDropdown.bottomOffset = CGPoint(x: 0, y: (fromCurrencyDropdown.anchorView?.plainView.bounds.height)!)
+        fromCurrencyDropdown.topOffset = CGPoint(x: 0, y:-((fromCurrencyDropdown.anchorView?.plainView.bounds.height)!))
+        fromCurrencyDropdown.direction = .bottom
+
+        targetCurrencyDropdown.bottomOffset = CGPoint(x: 0, y:(targetCurrencyDropdown.anchorView?.plainView.bounds.height)!)
+        targetCurrencyDropdown.topOffset = CGPoint(x: 0, y:-(targetCurrencyDropdown.anchorView?.plainView.bounds.height)!)
+        targetCurrencyDropdown.direction = .bottom
+        
+        
+        fromCurrencyDropdownView.isUserInteractionEnabled = true
+        
+        fromCurrencyDropdownView.addGestureRecognizer(UIGestureRecognizer(target: self, action: #selector(didTapFromCurrencyDropdown)))
+        
+        targetCurrencyDropdownView.addGestureRecognizer(UIGestureRecognizer(target: self, action: #selector(didTapTargetCurrencyDropdown)))
+        
     }
 
-
+    @IBAction func didTapConvertButton(_ sender: UIButton) {
+        
+    }
+        
+    @objc func didTapFromCurrencyDropdown() {
+        fromCurrencyDropdown.show(onTopOf: nil, beforeTransform: nil, anchorPoint: nil)
+    }
+    
+    @objc func didTapTargetCurrencyDropdown() {
+        targetCurrencyDropdown.show()
+    }
+    
 }
 
---
+++ CurrencyConverter/CustomExtensions.swift
@@ -0,0 +1,162 @@
+//
+//  UIControls Extensions to customise your 
+//  UI controls in xcode interface builder
+//
+
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+}
+
+//  Use this subclasses instead of the 
+//  UIKit parent to get the extensions above.
+@IBDesignable
+class DesignableView: UIView {}
+
+@IBDesignable
+class DesignableButton: UIButton {}
+
+@IBDesignable
+class DesignableLabel: UILabel {
+    func setLineHeight(lineHeight: CGFloat) {
+        let text = self.text
+        if let text = text {
+            let attributeString = NSMutableAttributedString(string: text)
+            let style = NSMutableParagraphStyle()
+            
+            style.lineSpacing = lineHeight
+            attributeString.addAttribute(NSAttributedString.Key.paragraphStyle, value: style, range: NSMakeRange(0, text.count))
+        }
+    }
+}
+
+@IBDesignable
+class EdgeInsetLabel: UILabel {
+    var textInsets = UIEdgeInsets.zero {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let insetRect = UIEdgeInsetsInsetRect(bounds, textInsets)
+        let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
+        let invertedInsets = UIEdgeInsets(top: -textInsets.top,
+                                          left: -textInsets.left,
+                                          bottom: -textInsets.bottom,
+                                          right: -textInsets.right)
+        return UIEdgeInsetsInsetRect(textRect, invertedInsets)
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, textInsets))
+    }
+}
+
+extension EdgeInsetLabel {
+    @IBInspectable
+    var leftTextInset: CGFloat {
+        set { textInsets.left = newValue }
+        get { return textInsets.left }
+    }
+    
+    @IBInspectable
+    var rightTextInset: CGFloat {
+        set { textInsets.right = newValue }
+        get { return textInsets.right }
+    }
+    
+    @IBInspectable
+    var topTextInset: CGFloat {
+        set { textInsets.top = newValue }
+        get { return textInsets.top }
+    }
+    
+    @IBInspectable
+    var bottomTextInset: CGFloat {
+        set { textInsets.bottom = newValue }
+        get { return textInsets.bottom }
+    }
+}
